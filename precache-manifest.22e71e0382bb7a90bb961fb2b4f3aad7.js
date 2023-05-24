bdaab7acb714ae23235fb8e5c9869f7df91a2a0c
Jared Kroke <jared.kroke@gmail.com>
2023-05-24 06:11:33 -0500

    Updates

---
+++ precache-manifest.22e71e0382bb7a90bb961fb2b4f3aad7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c81035c2856d2b2fd205",
+    "url": "/static/css/main.ecef6589.chunk.css"
+  },
+  {
+    "revision": "c81035c2856d2b2fd205",
+    "url": "/static/js/main.c81035c2.chunk.js"
+  },
+  {
+    "revision": "3f5a8f4c7bc0532f173e",
+    "url": "/static/js/1.3f5a8f4c.chunk.js"
+  },
+  {
+    "revision": "cddc92c48fb4db0237a8",
+    "url": "/static/js/2.cddc92c4.chunk.js"
+  },
+  {
+    "revision": "38973534419a972cf74f",
+    "url": "/static/js/runtime~main.38973534.js"
+  },
+  {
+    "revision": "f4745f6451b995031abf4e6bd2f314cd",
+    "url": "/static/media/galaxies.f4745f64.JPG"
+  },
+  {
+    "revision": "38e333c6f9828d14c72d4efe840bf6f5",
+    "url": "/index.html"
+  }
+];
